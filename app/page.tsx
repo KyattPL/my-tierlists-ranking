d8cf2e13d5794f365dc52d60e7dc5b19fe4ed652
KyattPL <siema1234@gazeta.pl>
2026-02-28 23:06:48 +0100

    Fixed force-dynamic for static export

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import { getAllTierlists } from "@/lib/data";
 import TierlistApp from "@/components/TierlistApp";
 
-export const dynamic = 'force-dynamic'; // Since we are reading local files that might change
-
 export default async function Page() {
   const data = await getAllTierlists();
   
